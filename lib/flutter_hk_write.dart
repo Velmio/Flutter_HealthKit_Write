def5abadf1a1f82897046802a0068025fe340999
Elliot Tikhomirov <tikhomirovi@icloud.com>
2020-07-22 15:36:31 +0300

    Add missing flutter bridge

--- lib/flutter_hk_write.dart
+++ lib/flutter_hk_write.dart
@@ -3,27 +3,30 @@ import 'dart:async';
 import 'package:flutter/services.dart';
 
 class FlutterHkWrite {
-  static const MethodChannel _channel =
-      const MethodChannel('flutter_hk_write');
-
+  static const MethodChannel _channel = const MethodChannel('flutter_hk_write');
 
   static Future<bool> get requestWritePermissions async {
-    final bool permission = await _channel.invokeMethod('requestWritePermissions');
+    final bool permission =
+        await _channel.invokeMethod('requestWritePermissions');
+    return permission;
+  }
+
+  static Future<bool> requestReadWritePermissionsForTypes(
+      List<String> types) async {
+    final bool permission = await _channel
+        .invokeMethod('requestReadWritePermissionsForTypes', {'types': types});
     return permission;
   }
 
   static Future<bool> writeSleepEntry(int from, int to) async {
-    final bool permission = await _channel.invokeMethod('writeSleepEntry', {
-      'from': from,
-      'to' : to
-    });
+    final bool permission = await _channel
+        .invokeMethod('writeSleepEntry', {'from': from, 'to': to});
     return permission;
-  } 
+  }
 
   static Future<bool> writeQuantityEntries(List<Map> entries) async {
-    final bool permission = await _channel.invokeMethod('writeQuantityEntries', {
-      'entries': entries
-    });
+    final bool permission = await _channel
+        .invokeMethod('writeQuantityEntries', {'entries': entries});
     return permission;
-  } 
+  }
 }
